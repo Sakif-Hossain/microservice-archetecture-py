33ff4615dce56e92bd9241b62e55416842a18823
SheikhSH <sakifhossain71@gmail.com>
2024-05-08 13:49:24 +0600

    docker and kubernetese

---
+++ python/src/gateway/server.py
@@ -0,0 +1,19 @@
+# library imports
+import os, pika, json, gridfs # gridfs is a library for storing large files in MongoDB
+                              # pika is a library for RabbitMQ to store messages  
+from flask import Flask, request
+from flask_pymongo import PyMongo
+# user imports
+from auth import validate
+from auth_service import access
+from storage import util
+
+server = Flask(__name__)
+server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos" # Endpoint for MongoDB
+
+mongo = PyMongo(server)
+
+fs = gridfs.GridFS(mongo.db) # GridFS object to store large files in MongoDB in chunks
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq")) # Connection to RabbitMQ
+channel = connection.channel()
