3e90767b2fac6c7dce8496deb575881a3665178a
SheikhSH <sakifhossain71@gmail.com>
2024-05-09 23:46:14 +0600

    uploading file and storing in the db

---
+++ python/src/gateway/auth/validate.py
@@ -0,0 +1,17 @@
+import os, requests
+
+def validate(request):
+    if not "Authorization" in request.headers:
+        return None, ("No credentials provided", 401)
+    
+    token = request.headers["Authorization"] # Get the JWT token from the request headers
+
+    if not token:
+        return None, ("No credentials provided", 401)
+    
+    response = requests.post(f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/validate", headers={"Authorization": token}) # Send a POST request to the auth service to validate the token
+
+    if response.status_code == 200:
+        return response.text, None # response.text contains the json object of the payload of the JWT token
+    else:
+        return None, (response.text, response.status_code)
---
+++ python/src/gateway/storage/util.py
@@ -0,0 +1,36 @@
+import pika, json
+
+def upload(file, fs, access, channel):
+    """
+    The function uploads the file to MongoDB using GridFS, sends a message to RabbitMQ so that downstream services can process the file,
+    by pulling it from MongoDB. This allows the asynchronous flow between the gateway and the service that processes the video file.
+
+    1. Upload the file to MongoDB using GridFS
+    2. Send a message to RabbitMQ to process the file when the file is uploaded successfully
+
+    Args:
+        file (werkzeug.datastructures.FileStorage): The file to be uploaded
+        fs (gridfs.GridFS): The GridFS object to store the file in MongoDB
+        access (dict): The JSON object of the payload of the JWT token
+        channel (pika.channel.Channel): The channel object to send messages to RabbitMQ
+
+    returns:
+        str: Error message if the file is not uploaded successfully, None otherwise
+    """
+    try:
+        # Save the file in MongoDB
+        file_id = fs.put(file)
+    except Exception as e:
+        return "internal server error", 500
+    
+    # Send a message to RabbitMQ to process the file
+    message = {
+        "video_file_id": str(file_id),
+        "mp3_file_id": None,
+        "username": access["username"]
+    }
+
+    try:
+        channel.basic_publish(exchange="", routing_key="video_processing", body=json.dumps(message))
+    except Exception as e:
+        return "internal server error", 500
